964e3d9f4acd52a8d0c64235bf931a5413342911
bogdan801 <bogdan2002801@gmail.com>
2023-08-01 23:46:58 +0300

    Added calculation of an area of a shape on map

--- app/src/main/java/com/bogdan801/digitalfarmer/presentation/screens/fields/FieldsScreen.kt
+++ app/src/main/java/com/bogdan801/digitalfarmer/presentation/screens/fields/FieldsScreen.kt
@@ -1,15 +1,20 @@
 package com.bogdan801.digitalfarmer.presentation.screens.fields
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
+import com.google.maps.android.SphericalUtil
 
 @Composable
 fun FieldsScreen(
@@ -22,59 +27,51 @@ fun FieldsScreen(
     val state by viewModel.screenState.collectAsStateWithLifecycle()
 
     Column(modifier = Modifier.fillMaxSize()) {
-        val singapore = LatLng(1.35, 103.87)
+        val europe = LatLng(49.0, 14.0)
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+            position = CameraPosition.fromLatLngZoom(europe, 3.5f)
         }
         val properties by remember {
             mutableStateOf(MapProperties(mapType = MapType.HYBRID))
         }
+
+        var shape by remember { mutableStateOf(listOf<LatLng>()) }
+        val area by remember { derivedStateOf { SphericalUtil.computeArea(shape).toInt() } }
         GoogleMap(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
             properties = properties,
             cameraPositionState = cameraPositionState,
+            onMapLongClick = {
+                shape = shape + listOf(it)
+            }
+        ){
+            shape.forEach{
+                Marker(state = MarkerState(position = it))
+
+            }
+            Polyline(points = shape)
+            /*if(area != null) {
+                Polygon(
+                    points = shape,
+                    fillColor = Color.Transparent,
+                    strokeColor = MaterialTheme.colorScheme.primary,
+                    strokeWidth = 10f
+                )
+            }*/
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+            Text(text = area.toString())
+
+            Button(onClick = { shape = listOf() }) {
+                Text(text = "Clear")
+            }
         }
     }
-
-
-    /*Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        userData?.profilePictureUrl?.let { url ->
-            AsyncImage(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                model = url,
-                contentDescription = "Profile picture",
-                contentScale = ContentScale.Crop
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = userData?.username.toString())
-        Text(text = userData?.userID.toString())
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(
-            onClick = {
-                scope.launch {
-                    viewModel.authUIClient.signOut()
-                    Toast.makeText(context, "Signed out", Toast.LENGTH_SHORT).show()
-                    navController.navigate(Screen.SignInScreen.route){
-                        popUpTo(0)
-                    }
-                }
-            }
-        ) {
-            Text(text = "Sign out")
-        }
-    }*/
-
 }
