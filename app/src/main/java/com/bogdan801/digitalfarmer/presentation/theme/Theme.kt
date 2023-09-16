7131fc1f7f4158f835ed4882a032df5fc8de91b4
bogdan801 <bogdan2002801@gmail.com>
2023-09-16 11:04:35 +0300

    Fixed app bars color

--- app/src/main/java/com/bogdan801/digitalfarmer/presentation/theme/Theme.kt
+++ app/src/main/java/com/bogdan801/digitalfarmer/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bogdan801.digitalfarmer.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,13 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
+private val darkScheme = darkColorScheme(
     primary = Brown80,
     secondary = DarkBrown80,
     tertiary = Green80,
@@ -46,7 +42,7 @@ private val DarkColorScheme = darkColorScheme(
     background = Gray6
 )
 
-private val LightColorScheme = lightColorScheme(
+private val lightScheme = lightColorScheme(
     primary = Brown40,
     secondary = DarkBrown20,
     tertiary = Green20,
@@ -80,7 +76,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun DigitalFarmerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -89,17 +84,12 @@ fun DigitalFarmerTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkScheme
+        else -> lightScheme
     }
 
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(colorScheme.surface)
 
     MaterialTheme(
         colorScheme = colorScheme,
